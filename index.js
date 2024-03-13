b3e79d9b954405cfd054de8ec7e774476bdf01cb
Nischitha sanchi <nischitha1320@gmail.com>
2024-03-13 22:24:36 +0530

    README update

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ app.use("/API", ApiRouter);
 
 app.get("/", (req, res) => {
   res.status(200)
-    .send("working well");
-});
+    .send(<h1>1.Get all Rooms with the endPoint /API/RoomApi
+
+      2. Listing all rooms with booked data with the endPoint /API/CustomerDetails
+
+      3.By using POST call create a room with the endPoint /API/CreateNewDetails
+
+      4. Deleting the rooms with the endPoint /API/Delete/:id
+
+      5. By using PUT call booking new hall with the endPoint /API/CreateNewHall/:id;
+    </h1>)});
 
 app.listen(PORT, () => {
   console.log(`App is working in the port ${PORT}`);
