342d79a66c5105110f7045f9546abda2d1e54429
Nischitha sanchi <nischitha1320@gmail.com>
2024-03-13 22:38:20 +0530

    indexjs changes

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.use("/API", ApiRouter);
 
 app.get("/", (req, res) => {
   res.status(200)
-    .send(<h1>1.Get all Rooms with the endPoint /API/RoomApi
+    .send(`<h1><li>1.Get all Rooms with the endPoint / API / RoomApi</li></br>
 
-      2. Listing all rooms with booked data with the endPoint /API/CustomerDetails
+  <li>2. Listing all rooms with booked data with the endPoint / API / CustomerDetails</li></br>
 
-      3.By using POST call create a room with the endPoint /API/CreateNewDetails
+  <li>3.By using POST call create a room with the endPoint / API / CreateNewDetails</li></br>
 
-      4. Deleting the rooms with the endPoint /API/Delete/:id
+  <li>4. Deleting the rooms with the endPoint / API / Delete /: id</li></br>
 
-      5. By using PUT call booking new hall with the endPoint /API/CreateNewHall/:id;
-    </h1>)});
+  <li>5. By using PUT call booking new hall with the endPoint / API / CreateNewHall /: id;</li></br>
+</h1>`);});
 
 app.listen(PORT, () => {
   console.log(`App is working in the port ${PORT}`);
